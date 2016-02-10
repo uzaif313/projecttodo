b6cac4714f4a5258091f107bbe714cd875e9a496
uzaif313 <uzaif313@gmail.com>
2016-02-10 13:29:29 +0530

    Proper UI Setup at Admin side for User and Project Module CRUD

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,31 @@
 class Project < ActiveRecord::Base
   has_many :todos
+  validates :project_name,:project_desc,:project_type,presence:true
 
+  rails_admin do
+    create do
+      field :project_name do
+        help "Please Enter Project name"
+      end
+      field :project_desc do
+        help "Please Enter Project Description"
+      end
+      field :project_type do
+        help "Please Enter Project Type"
+      end
+    end
+
+    edit do
+      field :project_name do
+        help "Please Enter Project name"
+      end
+      field :project_desc do
+        help "Please Enter Project Description"
+      end
+      field :project_type do
+        help "Please Enter Project Type"
+      end
+    end
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,44 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :todos
+  rails_admin do
+    create do
+      field :username do
+        help "Please Enter Username"
+      end
+      field :email do
+        help "Please Enter Email Address"
+      end
+      field :role,:enum do
+        help "Please Select Userrole"
+        enum  [:user,:admin]
+      end
+      field :password do
+        help "Please Enter Password"
+      end
+      field :password_confirmation do
+        help "Please Re-enter Password"
+      end
+    end
 
+    edit do
+      field :username do
+        help "Please Enter Username"
+      end
+      field :email do
+        help "Please Enter Email Address"
+      end
+      field :role,:enum do
+        help "Please Select Userrole"
+        enum  [:user,:admin]
+      end
+      field :password do
+        help "Please Enter Password"
+      end
+      field :password_confirmation do
+        help "Please Re-enter Password"
+      end
+
+    end
+  end
 end
